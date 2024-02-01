1875dafd3510067dc04e57441c4de2315f6894c1
DanJX <dannycanul282@gmail.com>
2024-01-31 21:00:50 -0500

    styles finales finales

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,33 +6,35 @@ const lastname: Ref<string> = ref('');
 const genero: Ref<string> = ref('');
 const age: Ref<number> = ref(0);
 
-const errors = ref([{
-
-}
+const errors: any = ref([
 ]);
 
 const validations = () => {
-
+  limpiar();
   // NAME
   if (name.value.length > 18) {
-    errors.value.push({ name: "mucho texto" });
+    errors.value.push("mucho texto");
   }
 
   if (name.value.length < 5) {
-    errors.value.push({ name: "poco texto" });
+    errors.value.push("poco texto");
   }
   // LASTNAME
   if (lastname.value.length == name.value.length) {
-    errors.value.push({ name: "NO PUEDES PONER TU NOMBRE EN EL APELLIDO" });
+    errors.value.push("No utilices tu nombre");
   }
   // AGE
   if (age.value > 60) {
-    errors.value.push({ age: "mucho edad" });
+    errors.value.push("mucho edad");
   }
 
   if (age.value < 0) {
-    errors.value.push({ age: "poca edad" });
+    errors.value.push("poca edad");
   }
+
+}
+const limpiar = () => {
+  errors.value = []
 }
 </script>
 
@@ -41,6 +43,13 @@ const validations = () => {
     <form>
       <div>
         <h1 class="mayu">Formulario </h1>
+      </div>
+      <!-- ERRORES -->
+      <div class="error">
+        <h3>Mensajes :</h3>
+        <span v-for="(error, index) in errors" :key="index">
+          {{ error }} <br>
+        </span>
       </div>
       <!-- NAME -->
       <div>
@@ -64,14 +73,9 @@ const validations = () => {
         </select>
         <input v-if="genero === 'otro'" type="text" placeholder="Especifica otro género">
       </div>
-      <button @submit="validations()">Enviar</button>
+      <!-- BTN -->
+      <button>Enviar</button>
     </form>
-    <div class="error">
-      <h3>Errors :</h3>
-      <span v-for="(error, index) in errors" :key="index">
-        {{ error }}
-      </span>
-    </div>
   </main>
 </template>
 
@@ -84,16 +88,47 @@ form {
   display: flex;
   flex-direction: column;
   gap: 20px;
+  box-shadow: 5px 2px 2px 1px #41b883;
+}
+
+form div input {
+  height: 45px;
+  border-radius: 15px;
+  border: 1px solid #41b883;
+}
+
+select {
+  height: 45px;
+  background: #f9f9f9;
+  border-radius: 15px;
+}
+
+button {
+  cursor: pointer;
+  font-weight: bold;
+  background: #41b883;
+  height: 45px;
+  color: white;
+  border-radius: 15px;
+}
+
+button:hover {
+  background: transparent;
+  border: 1px solid #41b883;
+  color: #41b883;
 }
 
 .mayu {
   text-transform: uppercase;
   color: #0c0c0c;
+  font-size: 20PX;
 }
 
 .error {
+  margin-top: 10px;
   text-align: center;
-  color: rgb(255, 255, 255);
-  background: rgb(255, 0, 0);
+  color: rgb(0, 0, 0);
+  border: 1px solid red;
+  border-radius: 15px;
 }
 </style>
